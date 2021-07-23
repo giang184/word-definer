c6e5cc8fa91b4fad5dc96cd5f2e1773aabdd272f
Andrew Giang <giang184@gmail.com>
2021-07-23 10:06:24 -0700

    pass definition test for save and find

--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -41,4 +41,22 @@ describe '#Definition' do
     end
   end
 
+  describe('#save') do
+    it("saves a definition") do
+      def1 = Definition.new("ball", @word.id, nil)
+      def1.save()
+      expect(Definition.all).to(eq([def1]))
+    end
+  end
+
+  describe('.find') do
+    it("finds a definition by id") do
+      def1 = Definition.new("ball", @word.id, nil)
+      def1.save()
+      def2 = Definition.new("game of kick", @word.id, nil)
+      def2.save()
+      expect(Definition.find(def2.id)).to(eq(def2))
+    end
+  end
+
 end
