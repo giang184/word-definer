e5740364ed5c58b55665f89ac098c87bf9acbc0f
Andrew Giang <giang184@gmail.com>
2021-07-23 10:50:50 -0700

    integration test for add a word

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -16,3 +16,13 @@ describe('visits home page', {:type => :feature}) do
     expect(page).to have_content('Your Own Dictionary')
   end
 end
+
+describe('visits a word page', {:type => :feature}) do
+  it('should visit a word page when clicked on a word') do
+    visit('/words')
+    click_on('Add a new word')
+    fill_in('word_name', :with => 'local')
+    click_on('Go!')
+    expect(page).to have_content('local')
+  end
+end
