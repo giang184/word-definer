af0cb45689e0042f0d00271d7ed02935d4942cd8
Andrew Giang <giang184@gmail.com>
2021-07-23 10:56:31 -0700

    integration test visiting a word page

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -17,12 +17,23 @@ describe('visits home page', {:type => :feature}) do
   end
 end
 
-describe('visits a word page', {:type => :feature}) do
-  it('should visit a word page when clicked on a word') do
+describe('add a word', {:type => :feature}) do
+  it('should add a word and return to homepage') do
     visit('/words')
     click_on('Add a new word')
     fill_in('word_name', :with => 'local')
     click_on('Go!')
     expect(page).to have_content('local')
+  end
+end
+
+describe('visits a word page', {:type => :feature}) do
+  it('should visit a word page when clicked on a word') do
+    visit('/words')
+    click_on('Add a new word')
+    fill_in('word_name', :with => 'hello')
+    click_on('Go!')
+    click_on('hello')
+    expect(page).to have_content('Word: hello')
   end
 end
