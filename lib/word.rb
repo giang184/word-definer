12be4c7c507b5f30fbd06e298a71f36e82399847
Andrew Giang <giang184@gmail.com>
2021-07-23 09:37:28 -0700

    pass test for sort

--- lib/word.rb
+++ lib/word.rb
@@ -38,4 +38,22 @@ class Word
     @@words.delete(self.id)
   end
 
+  def self.search(name)
+    @@words.select{|key, value| value.name == name}
+  end
+
+  def self.sort
+    #for each item in the hash, retrieve the names and push into array. Sort the array. Then search for each names in the array (which returns Album object), push that into a new Hash. Reassign
+    temp = @@words.sort_by { |key, val| val.name.downcase }
+    temp_hash = {}
+    temp.each { |element|
+      temp_hash[element[0]] = element[1]
+    }
+    temp_hash
+  end
+
+  def definitions
+    Definition.find_by_word(self.id)
+  end
+
 end
