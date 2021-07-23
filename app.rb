7748466d04c49b2608fa565b717556d6542edab5
Andrew Giang <giang184@gmail.com>
2021-07-23 10:31:05 -0700

    add homepage

--- app.rb
+++ app.rb
@@ -1,8 +1,11 @@
-require 'sinatra'
-require 'sinatra/reloader'
-also_reload 'lib/**/*.rb'
-require 'pry'
+require('sinatra')
+require('sinatra/reloader')
+require('./lib/word')
+require('./lib/definition')
+require('pry')
+also_reload('lib/**/*.rb')
 
 get '/' do
-  "Hello word!"
+  @words = Word.all
+  erb(:words)
 end
