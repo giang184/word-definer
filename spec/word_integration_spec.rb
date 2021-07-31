54d55fb12d4ed88abe5c212d0aa848922b544bf5
Andrew Giang <giang184@gmail.com>
2021-07-31 09:30:32 -0700

    add separate integration tests for definitions

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -70,12 +70,25 @@ describe('sort the word list', {:type => :feature}) do
     fill_in('word_name', :with => 'hi')
     click_on('Go!')
     click_on('View Sorted List')
-    expect(page).to have_content('Words sorted by alphabetical order')
+    expect(page).to have_content('Words and their definitions sorted by alphabetical order')
   end
 end
 
-describe('add a definition to the list and update it', {:type => :feature}) do
-  it('should sort the word list') do
+describe('delete a word', {:type => :feature}) do
+  it('should delete a word') do
+    visit('/words')
+    click_on('Add a new word')
+    fill_in('word_name', :with => 'good')
+    click_on('Go!')
+    click_on('good')
+    click_on('Edit word')
+    click_on('Delete word')
+    expect(page).to_not have_content('good')
+  end
+end
+
+describe('add a definition', {:type => :feature}) do
+  it('should add a definition') do
     visit('/words')
     click_on('Add a new word')
     fill_in('word_name', :with => 'swimming')
@@ -83,9 +96,37 @@ describe('add a definition to the list and update it', {:type => :feature}) do
     click_on('swimming')
     fill_in('definition_name', :with => 'you paddle with your arms')
     click_on('Add')
-    click_on('you paddle with your arms')
-    fill_in('name', :with => 'you paddle with your legs')
+    expect(page).to have_content('you paddle with your arms')
+  end
+end
+
+describe('update a definition', {:type => :feature}) do
+  it('should update a definition') do
+    visit('/words')
+    click_on('Add a new word')
+    fill_in('word_name', :with => 'jogging')
+    click_on('Go!')
+    click_on('jogging')
+    fill_in('definition_name', :with => 'run slowly')
+    click_on('Add')
+    click_on('run slowly')
+    fill_in('name', :with => 'run slowly with your legs')
     click_on('Update')
-    expect(page).to have_content('you paddle with your legs')
+    expect(page).to have_content('run slowly with your legs')
+  end
+end
+
+describe('delete a definition', {:type => :feature}) do
+  it('should delete a definition') do
+    visit('/words')
+    click_on('Add a new word')
+    fill_in('word_name', :with => 'greetings')
+    click_on('Go!')
+    click_on('greetings')
+    fill_in('definition_name', :with => 'hello to someone')
+    click_on('Add')
+    click_on('hello to someone')
+    click_on('Delete definition')
+    expect(page).to_not have_content('hello to someone')
   end
 end
